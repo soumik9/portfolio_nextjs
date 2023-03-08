a093b55a81ed1f7e77c442d244e71c50278b09bf
Soumik <soumik.ahammed.9@gmail.com>
2023-03-08 16:21:21 +0600

    slider data added

--- src/views/landing/Home/components/HReviews/partials/HRSlider.tsx
+++ src/views/landing/Home/components/HReviews/partials/HRSlider.tsx
@@ -5,6 +5,7 @@ import { reviewSlidersData } from '@config/constants';
 import Image from 'next/image';
 // import "swiper/css/pagination";
 // import { Pagination } from "swiper";
+const parse = require('html-react-parser');
 
 type Props = {}
 
@@ -21,16 +22,16 @@ const HRSlider = (props: Props) => {
             >
                 {reviewSlidersData.map((slideContent, index) => (
                     <SwiperSlide key={slideContent._id} virtualIndex={index}>
-                        <div className='bg-lightDark p-[49px_40px] rounded-sm'>
-                            <p className='leading-[160%] text-purple'>{slideContent.review}</p>
+                        <div className='bg-lightDark p-[40px_25px_40px_40px] rounded-sm'>
+                            <p className='leading-[160%] text-purple '>{slideContent.review}</p>
 
                             <div className='mt-8 flex items-center gap-8'>
-                                <div className='border border-primary w-max rounded-full'>
-                                    <Image src={slideContent.img} width={60} height={60} alt={slideContent.name} />
-                                </div>
+
+                                <Image src={slideContent.img} width={60} height={60} alt={slideContent.name} className='rounded-full p-[1px] border border-primary object-cover' />
+
                                 <div>
                                     <h6 className='text-primary'>{slideContent.name}</h6>
-                                    <p className='text-secondary'>{slideContent.position}</p>
+                                    <p className='text-secondary'>{parse(slideContent.position)}</p>
                                 </div>
                             </div>
                         </div>
