bd6e69005ca3d6309bb3c341a568784091413f9d
soumik9 <soumik.ahammed.9@gmail.com>
2024-08-25 13:59:12 +0600

    service section & header animation

--- src/components/ViewsComp/SectionTop/SectionTop.tsx
+++ src/components/ViewsComp/SectionTop/SectionTop.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 type Props = {
     titleSpan: string;
@@ -10,9 +11,13 @@ const SectionTop = ({ titleSpan, title, subTitle }: Props) => {
     return (
         <center className='mb-[15px] md:mb-[25px] lg:mb-[60px]'>
             <h5 className='heading-h5 text-secondary uppercase'>{subTitle}</h5>
-            <h1 className='mt-[12px] heading-h1 text-secondary font-secondary capitalize'>
+            <motion.h1
+                className='mt-[12px] heading-h1 text-secondary font-secondary capitalize'
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.5 } }}
+            >
                 {title} <span className='text-primary'>{titleSpan}</span>
-            </h1>
+            </motion.h1>
         </center>
     )
 }
--- src/views/landing/Home/components/Services/Services.tsx
+++ src/views/landing/Home/components/Services/Services.tsx
@@ -3,6 +3,23 @@ import SectionTop from '@components/ViewsComp/SectionTop/SectionTop'
 import { serviceItems } from '@config/constants/services'
 import React from 'react'
 import { BsCheck } from 'react-icons/bs'
+import { motion } from "framer-motion"
+
+const serviceItemVariant = {
+    hidden: (index: number) => ({
+        opacity: 0,
+        x: 200
+    }),
+    show: (index: number) => ({
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: 0.3 * (index + 0.3),
+            duration: 0.5 * (index + 0.3),  // Longer duration for smoother effect
+            ease: 'easeIn',
+        },
+    }),
+};
 
 const Services = () => {
     return (
@@ -13,25 +30,36 @@ const Services = () => {
                 titleSpan='Services'
             />
 
-            <div className='grid lg:grid-cols-3 lg:gap-x-5 gap-y-5 lg:gap-y-0'>
+            <motion.div
+                className='grid lg:grid-cols-3 lg:gap-x-5 gap-y-5 lg:gap-y-0 overflow-hidden'
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: true }}
+            >
 
-                {serviceItems.map((service) => (<article key={service._id} className="service bg-lightDark border border-smartian trans hover:bg-bgDark cursor-default lg:min-h-[360px] xll:min-h-[0]">
+                {serviceItems.map((service, index) => (<motion.article
+                    custom={index}
+                    variants={serviceItemVariant}
+                    key={service._id}
+                    className="service bg-lightDark border border-smartian trans hover:bg-bgDark cursor-default lg:min-h-[360px] xll:min-h-[0]"
+                >
 
                     <div className="bg-smartian p-[1.5rem] service__header">
                         <h3 className='text-white text-[1rem] text-center'>{service.title}</h3>
                     </div>
 
                     <ul className="my-5 flex flex-col items-center lg:items-start gap-y-3">
-                        {service.lists.map((item: string, index: number) => <li className='flex gap-1.5 lg:ml-5' key={`serviceList${index}`}>
+                        {service.lists.map((item: string, index: number) => <li
+                            className='flex gap-1.5 lg:ml-5'
+                            key={`serviceList${index}`}
+                        >
                             <BsCheck className='text-primary text-[24px]' />
                             <p className='text-[0.9rem] tracking-wide'>{item}</p>
                         </li>)}
                     </ul>
-                </article>
+                </motion.article>
                 ))}
-            </div>
-
-
+            </motion.div>
         </SectionLayout>
     )
 }
